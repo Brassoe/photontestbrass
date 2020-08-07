96f6495f0b8deff84ddab29802cb26a09d306a4a
Christian Brassøe <Christianbrass@hotmail.com>
2020-08-07 16:52:16 +0200

    first commit

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Bolt;
+using Bolt.Matchmaking;
+using UdpKit;
+
+public class Menu : Bolt.GlobalEventListener
+{
+    public void StartServer()
+    {
+        BoltLauncher.StartServer();
+    }
+
+    public void StartClient()
+    {
+        BoltLauncher.StartClient();
+    }
+
+    public override void BoltStartDone()
+    {
+        if (BoltNetwork.IsServer)
+        {
+            string matchName = "Test Match";
+
+            BoltMatchmaking.CreateSession(
+                sessionID: matchName,
+                sceneToLoad: "SampleScene"
+            );
+        }
+
+    }
+
+    public override void SessionListUpdated(Map<Guid, UdpSession> sessionList)
+    {
+        foreach ( var session in sessionList )
+        {
+            UdpSession photonSession = session.Value as UdpSession;
+
+            if (photonSession.Source == UdpSessionSource.Photon)
+            {
+                BoltMatchmaking.JoinSession(photonSession);
+            }
+        }
+
+
+
+    }
+}
